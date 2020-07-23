bb8e869d3101a48e1536bcd27a576b1caf7b0932
Msswamycreate <57250315+Msswamycreate@users.noreply.github.com>
2020-07-23 18:11:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Appointment from './components/Appointment';
+import Contact from './components/Contact';
+import Docter from './components/Docter';
+import Hospital from './components/Hospital';
+
+import Navbar from './components/CustomNavbar';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/hospital" component={Hospital} />
+        <Route path="/docter" component={Docter} />
+        <Route path="/appointment" component={Appointment} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
